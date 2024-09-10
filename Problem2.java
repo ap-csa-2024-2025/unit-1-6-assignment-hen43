a1f8df0a64354fc0721b9ee72e3d1744cc5c7126
hen43 <whshle08@gmail.com>
2024-09-10 11:22:14 -0700

    committing commit
    goonmaxxing

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,12 @@
+public class Problem2{
+    static int round(double x){
+        return((int)(x+0.5));
+    } 
 
+    public static void main(String[] args){
+        double gyatt = 2.3;
+        double ohio = 6.5;
+
+        System.out.println(round(gyatt) + round(ohio));
+    }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,8 @@
+public class Problem3{
+    public static void main(String[] args){
+        double sigma = 4.6232;
+        double goonyman = sigma % 1;
 
+        System.out.println((int)(goonyman * 1000));
+    }
+}
